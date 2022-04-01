61c88b1faad4db4fdd24854019696d8424983306
Carlin <carlin.d.mitchell@gmail.com>
2022-03-31 21:04:56 -0500

    add roll command validation

--- dice-roller.py
+++ dice-roller.py
@@ -4,12 +4,14 @@ import re
 def roll_die(num_sides):
     return random.randint(1, num_sides)
 
-
 while True:
     user_choice = input("ROLL: ")
     print("")
     
     if not re.match(r'[1-9]+[dD][1-9]+', user_choice):
+        print('EXITING PROGRAM...')
+        print('Valid choices use this syntax: <NUMBER OF DICE>d<SIDES PER DIE>\nExample: 1d20 (rolls 1 die with 20 sides each)')
+        print()
         break
 
     split_choice = user_choice.split('d')
@@ -22,8 +24,9 @@ while True:
     for i in range(number_of_dice):
         roll = roll_die(sides_per_die)
         rolls.append(roll)
-        print(f"roll #{i+1}:\n{roll}\n")
-    print(f"\nTotal: {sum(rolls)}")
+        print(f"roll #{i+1}:\n{roll}")
+    print("")
+    print(f"Total: {sum(rolls)}")
     print(f"Average: {int(sum(rolls)/len(rolls))}")
     print('\n'*2)
 
