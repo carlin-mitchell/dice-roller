be855842fb9d3e5e59b88e6251ab953563f4c7d1
Carlin Mitchell <carlinmi@umn.edu>
2022-04-01 13:29:03 -0500

    updated print function formatting

--- dice-roller.py
+++ dice-roller.py
@@ -5,12 +5,13 @@ def roll_die(num_sides):
     return random.randint(1, num_sides)
 
 while True:
-    user_choice = input("ROLL: ")
+    user_choice = input("ROLL: ").lower()
     print("")
     
-    if not re.match(r'[1-9]+[dD][1-9]+', user_choice):
+    if not re.match(r'[1-9]+[d][1-9]+', user_choice):
         print('EXITING PROGRAM...')
-        print('Valid choices use this syntax: <NUMBER OF DICE>d<SIDES PER DIE>\nExample: 1d20 (rolls 1 die with 20 sides each)')
+        print('Valid choices use this syntax: <NUMBER OF DICE>d<SIDES PER DIE>\n\
+        Examples: 1d20 (rolls 1 die with 20 sides each); 3d8 (rolls 3 dice with 8 sides each')
         print()
         break
 
@@ -24,7 +25,7 @@ while True:
     for i in range(number_of_dice):
         roll = roll_die(sides_per_die)
         rolls.append(roll)
-        print(f"roll #{i+1}:\n{roll}")
+        print(f"Roll #{i+1}:\n{roll}\n")
     print("")
     print(f"Total: {sum(rolls)}")
     print(f"Average: {int(sum(rolls)/len(rolls))}")
